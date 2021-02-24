e5edfc24a1f7af9521311db19e69f8aec4441703
Andrii <andrii.o@ukr.net>
2021-02-25 00:46:07 +0200

    1

--- myJs.js
+++ myJs.js
@@ -1,26 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	$('#fullpage').fullpage({
 		//options here
-		autoScrolling:true,
-        scrollHorizontally: false,
-        verticalCentered: false,
-		navigation: true
-	});
+		autoScrolling: true,
+		scrollHorizontally: false,
+		verticalCentered: false,
+		navigation: true,
 
-    
-});
+	})
+})
 
-var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
+
+let typed = new Typed('#typed', {
+	stringsElement: '#typed-strings',
 	typeSpeed: 50
-  });
+})
 
-function preload () {
-		let preloader = document.querySelector('.preloader');
-		preloader.style.display = 'none'
+
+function preload() {
+	let preloader = document.querySelector('.preloader')
+	preloader.style.display = 'none'
 }
+setTimeout(preload, 3000)
 
-setTimeout(preload, 3000);
 
-
-      
