e1d4a5a3a489be621e373dbab3aadeba472e2670
Andrii <andrii.o@ukr.net>
2021-02-27 18:57:23 +0200

    waiting scroll for 2th section

--- myJs.js
+++ myJs.js
@@ -5,14 +5,22 @@ $(document).ready(function () {
 		scrollHorizontally: false,
 		verticalCentered: false,
 		navigation: true,
+		scrollingSpeed: 400,
 
+		onLeave: function(index, nextIndex, direction) {
+			
+			if(nextIndex == 2) {
+				$('.main__message').css('display', 'grid')
+			} 
+		}
 	})
+	
 })
 
 
 let typed = new Typed('#typed', {
 	stringsElement: '#typed-strings',
-	typeSpeed: 50
+	typeSpeed: 20
 })
 
 
@@ -23,3 +31,12 @@ function preload() {
 setTimeout(preload, 3000)
 
 
+// function scrollHandler() {
+// 	let point = document.querySelectorAll('#fp-nav ul li a')
+//     for(point; point < 3; point++) {
+// 		if(point.classList == 'active') {
+// 			alert(active)
+// 		}
+// 	}
+// }
+// scrollHandler()
